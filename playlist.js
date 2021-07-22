60982168ab592c96718111c877eff7b2a9ed4b39
Uriel Navarro <87386565+urielnara@users.noreply.github.com>
2021-07-22 15:57:26 -0500

    Create playlist.js

---
+++ playlist.js
@@ -0,0 +1,122 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<body>
+<body background="https://media1.tenor.com/images/803e41177a2e3290e816002227022345/tenor.gif?itemid=14618996">
+
+<div id="1" class="f1">
+<img alt="Twenty One Pilots"
+  title="Twenty One Pilots" src="https://media.giphy.com/media/8BkI7xINtblqDIOpyk/giphy.gif" height="390"
+  width="440" />
+</div>
+</body>
+</html>
+<table style="text-align: left; width: 100%;" border="1"
+ cellpadding="2" cellspacing="2">
+    <tr>
+      <td id="c" class="dod">Cancion</td>
+      <td id="cc" class="doe">Reproducir</td>
+    </tr>
+<tr>
+<td>Level Of Concern</td>
+<td><a href="https://www.youtube.com/channel/UCBQZwaNPFfJ1gZ1fLZpAEGw"><span class="blanco">Escuchar</span></a></td>
+</tr>
+
+<tr>
+<td>Chlorine</td>
+<td><a href="https://www.youtube.com/watch?v=eJnQBXmZ7Ek"><span class="blanco">Escuchar</span></a></td>
+</tr>
+
+<tr>
+<td>Stressed Out</td>
+<td><a href="https://www.youtube.com/watch?v=pXRviuL6vMY"><span class="blanco">Escuchar</span></a></td>
+</tr>
+
+<tr>
+<td>Ride</td>
+<td><a href="https://www.youtube.com/watch?v=Pw-0pbY9JeU"><span class="blanco">Escuchar</span></a></td>
+</tr>
+
+<tr>
+<td>Heathens</td>
+<td><a href="https://www.youtube.com/watch?v=UprcpdwuwCg"><span class="blanco">Escuchar</span></a></td>
+</tr>
+
+<tr>
+<td>Smithereens</td>
+<td><a href="https://www.youtube.com/watch?v=lcpVMHd9LXk"><span class="blanco">Escuchar</span></a></td>
+</tr>
+
+<tr>
+<td>Heavydirtysoul</td>
+<td><a href="https://www.youtube.com/watch?v=r_9Kf0D5BTs"><span class="blanco">Escuchar</span></a></td>
+</tr>
+
+<tr>
+<td>My Blood</td>
+<td><a href="https://www.youtube.com/channel/UCBQZwaNPFfJ1gZ1fLZpAEGw"><span class="blanco">Escuchar</span></a></td>
+</tr>
+
+<tr>
+<td>Migraine</td>
+<td><a href="https://www.youtube.com/watch?v=z-wYtUBBTuY"><span class="blanco">Escuchar</span></a></td>
+</tr>
+
+<tr>
+<td>House of Gold</td>
+<td><a href="https://www.youtube.com/watch?v=mDyxykpYeu8"><span class="blanco">Escuchar</span></a></td>
+</tr>
+
+<tr>
+<td>Jumpsuit</td>
+<td><a href="https://www.youtube.com/watch?v=UOUBW8bkjQ4"><span class="blanco">Escuchar</span></a></td>
+</tr>
+
+<tr>
+<td>Nico and the niners</td>
+<td><a href="https://www.youtube.com/watch?v=hMAPyGoqQVw"><span class="blanco">Escuchar</span></a></td>
+</tr>
+
+<tr>
+<td>We don´t believe whats on TV</td>
+<td><a href="https://www.youtube.com/watch?v=Xa9nJch8Ydk"><span class="blanco">Escuchar</span></a></td>
+</tr>
+
+<tr>
+<td>Holding On to You</td>
+<td><a href="https://www.youtu.be/ltBMxkLUIwY"><span class="blanco">Escuchar</span></a></td>
+</tr>
+
+<tr>
+<td>Lovely</td>
+<td><a href="https://www.youtube.com/watch?v=3C-2xCuJZYQ"><span class="blanco">Escuchar</span></a></td>
+</tr>
+
+<tr>
+<td>Fake you Out</td>
+<td><a href="https://www.youtube.com/watch?v=2sn0ZSq8-Rc"><span class="blanco">Escuchar</span></a></td>
+</tr>
+
+<tr>
+<td>Oh ms Believer </td>
+<td><a href="https://www.youtube.com/watch?v=IpiaYAGjXhU"><span class="blanco">Escuchar</span></a></td>
+</tr>
+
+<tr>
+<td>Forest</td>
+<td><a href="https://www.youtube.com/watch?v=5daYd91k4ws"><span class="blanco">Escuchar</span></a></td>
+</tr>
+
+<tr>
+<td>Legend</td>
+<td><a href="https://www.youtube.com/watch?v=f3bzqzspXPI"><span class="blanco">Escuchar</span></a></td>
+</tr>
+
+<tr>
+<td>The Run and Go</td>
+<td><a href="https://www.youtube.com/watch?v=qTUc91b9mxI"><span class="blanco">Escuchar</span></a></td>
+</tr>
+</table>
+
+
+`;
